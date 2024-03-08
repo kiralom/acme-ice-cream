75f2685c94a3fc4be25ed50770f877f474acc971
Kira <Kira@blanks-MacBook-Pro.local>
2024-03-08 16:45:51 -0500

    with readme and updated JS

---
+++ README.md
@@ -0,0 +1,33 @@
+#The Acme Ice Cream Shop
+
+# Overview
+
+In this workshop you will be building an API which will allow the Acme Ice Cream Shop to manage it's flavors.
+
+# Directions
+
+The goal of this workshop is to successfully create the following routes:
+
+- GET /api/flavors - returns array of flavors
+- GET /api/flavors/:id - returns a single flavor
+- POST /api/flavors - payload: the flavor to create, returns the created flavor
+- DELETE /api/flavors/:id - returns nothing
+- PUT /api/flavors/:id - payload: the updated flavor, returns the updated flavor
+
+# Flavor
+
+- id
+- name (STRING)
+- is_favorite (BOOLEAN)
+- created_at (TIMESTAMP)
+- updated_at (TIMESTAMP)
+
+# Steps
+
+- Create GitHub repository. Use the guided practice as a reference.
+- Create the flavors table. Consider the different columns you'll need to add as well as the most appropriate data types. Don't forget to add code to DROP tables before recreating them.".
+- Seed the table with some flavors of your choosing.
+- Create express server.
+- Have express server listen.
+- Test your routes using curl and/or Postman, again using guided practice as a reference.
+- Submit your work in the space provided.
--- index.js
+++ index.js
@@ -1,1 +1,105 @@
 console.log("Application Coming Soon!");
+
+const pg = require("pg");
+const client = new pg.Client(
+  process.env.DATABASE_URL || "postgres://localhost/"
+);
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use((req, res, next) => {});
+
+app.get("/", async (req, res, next) => {
+  try {
+    const SQL = `
+         SELECT *
+         FROM icecream
+         ORDER BY name
+        `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.post("/icecream", async (req, res, next) => {});
+//finish this
+
+app.post("/icecream/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+    UPDATE notes
+    SET name=$1, updated_at=now()
+    `;
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.get("/icecream/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+           SELECT *
+           FROM icecream
+           WHERE id = $1
+          `;
+    const response = await client.query(SQL, [req.params.id]);
+    res.send(response.rows[0]);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.delete("/icecream/:id", async (req, res, next) => {
+  try {
+    const SQL = `
+             DELETE
+             FROM icecream
+             WHERE id = $1
+            `;
+    await client.query(SQL, [req.params.id]);
+    res.sendStatus(204);
+  } catch (ex) {
+    next(ex);
+  }
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(err.status || 500).send({ message: err.message || err });
+});
+
+const init = async () => {
+  console.log("Connecting to DB...");
+  await client.connect();
+  console.log("Connected to DB!");
+  let SQL = `
+  DROP TABLE IF EXISTS icecream;
+  CREATE TABLE icecream(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR (100) NOT NULL, 
+    is_favorite BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT now(),
+    updated_at TIMESTAMP DEFAULT now()
+  );
+  `;
+  await client.query(SQL);
+  console.log("Tables Created!");
+
+  SQL = `
+    INSERT INTO icecream(name, is_favorite) VALUES('strawberry', true);
+    INSERT INTO icecream(name, is_favorite) VALUES('mint', false);
+  `;
+  await client.query(SQL);
+  console.log("Data seeded!");
+
+  const port = process.env.PORT | 3000;
+  app.listen(port, () => console.log(`Listening on port: ${port}`));
+};
+
+init();
